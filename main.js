ca149887fd1f11b9f1dd4d6fc8595e501c80cdd9
James <jameswj111@gmail.com>
2025-05-02 17:42:16 -0400

    initializes with programmitically created sky

---
+++ main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    console.log('document ready')
+    const createSky = () => {
+        console.log('createSky');
+    
+        createClouds();
+        createMoon();
+    }
+
+    const createClouds = () => {
+        console.log('createClouds');
+        const numPixels = Math.floor(Math.random() * 15);
+        const numClouds = Math.floor(Math.random() * 11);
+
+
+        
+        for (let i = 0; i <= numClouds; i++) {
+            let random = Math.random();
+            let pixel_size;
+            
+            if (random >= 0.28 && random <= 0.85) {
+                pixel_size = 40;
+            } else if (random < 0.28){
+                pixel_size = 20;
+            } else {
+                pixel_size = 10;
+            }   
+            console.log('creating cloud with pixel size:', pixel_size)
+            createSingleCloud(numPixels, pixel_size);
+        }
+    }
+    
+    const createMoon = () => {
+        console.log('createMoon');
+    }
+    
+    const createSingleCloud = (numPixels, pixel_size) => {
+        const cloudContainer = $("<div>");
+        cloudContainer.addClass('cloudContainer');
+        cloudContainer.css('bottom', '0');
+
+        for (let i = 0; i <= numPixels; i++) {
+            console.log('adding pixel')
+            const cluster_x = pixel_size * (Math.floor(Math.random(0, 4) * (5)));
+            const cluster_y = pixel_size * (Math.floor(Math.random(0, 4) * (5)));
+
+            const cloud_width = pixel_size * Math.ceil(Math.random() * 5);
+            const cloud_height = pixel_size * Math.ceil(Math.random() * 3);
+
+            const cloud_pixel = $("<div>");
+            cloud_pixel.addClass('cloudPixel');
+
+            cloudContainer.append(cloud_pixel);
+            
+            cloud_pixel.css('left', cluster_x);
+            cloud_pixel.css('bottom', cluster_y);
+
+            cloud_pixel.css('width', cloud_width + 1);
+            cloud_pixel.css('height', cloud_height + 1);
+        }
+        
+        $('#skyContainer').append(cloudContainer)
+        cloudContainer.css('left', 50 * Math.ceil(Math.random() * (window.innerWidth/50)))
+        cloudContainer.css('bottom', 50 * Math.floor(Math.random() * 3))
+    }
+     
+    createSky();
+})
